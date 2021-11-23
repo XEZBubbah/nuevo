2bf55c6582f65a9f9f368790ee09dd035cbf569c
XEZBubbah <yesuso16@hotmail.com>
2021-11-23 13:23:29 -0500

    Inicio del proyecto To Do

---
+++ nuevo/src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <div id= "header"></div>
+    <Search v-on:query-change="querySearch"/>
+    <div id= "main-container">
+      <h2>Todos</h2>
+      <Add v-on:add-todo="addTodo"/>
+      <Todos v-bind:todoslist="copyTodos"/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Search from './components/Search.vue';
+import Todos from './components/Todos.vue';
+import Add from './components/Add.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Todos, Add, Search
+  },
+  methods:{
+    deletes(id){
+      this.todos = this.todos.filter(todo => todo.id != id);
+      this.copyTodos = [...this.todos]
+    },
+    addTodo(todo){
+      this.todos.push(todo);
+      this.copyTodos = [...this.todos]
+    },
+    querySearch(query){
+     if(query.trim () == ''){
+       this.copyTodos = [... this.todos];
+     }else{
+       const temp = this.todos.filter(todo => {
+         return todo.title.includes(query)
+       });
+
+       this.copyTodos = [... temp]
+     }
+    }
+  },
+  data() {
+    return{
+      todos: [
+        {
+          id: 0,
+          title: 'Entrevista',
+          completed: false,
+        },
+        
+        {
+          id: 1,
+          title: 'Revisar Tarea',
+          completed: false,
+        },
+        
+        {
+          id: 2,
+          title: 'Escuchar Podcast',
+          completed: true,
+        },
+      ],
+      copyTodos: []
+    }
+  },
+  created(){
+    this.copyTodos = [... this.todos];
+  }
+}
+</script>
+
+<style>
+  *{
+    box-sizing: border-box;
+  }
+
+  body{
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    font-size: 1.5em;
+    padding: 0;
+    margin: 0;
+  }
+
+  #main-container{
+    border: solid 1px #ccc;
+    width: 600px;
+    margin: 100px auto;
+  }
+
+  #header{
+    background: black;
+    padding: 10px;
+  }
+
+  h2{
+    padding: 0 10px;
+  }
+</style>
